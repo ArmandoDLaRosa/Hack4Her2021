8a844470870605e116049cbf06ff2e237fea269e
Armando De La Rosa <37047175+ArmandoDLaRosa@users.noreply.github.com>
2021-06-20 11:40:20 -0500

    Add files via upload

---
+++ WebAPI/database/db.js
@@ -0,0 +1,45 @@
+var sql = require('mysql');
+
+var config = sql.createConnection({
+  host: "hack4her.c4hj7dzeiz1t.us-east-2.rds.amazonaws.com",
+  user: "dbadmin",
+  password: "kwTjn5G5Wt4u6GG4",
+  database: "hack4her_db"
+});
+
+config.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+});
+
+
+
+/*const sql = require('mysql');
+
+const config = {
+  user: 'dbadmin',
+  password: 'kwTjn5G5Wt4u6GG4',
+  database: 'hack4her',
+  server: 'hack4her.c4hj7dzeiz1t.us-east-2.rds.amazonaws.com',
+  options: {
+    trustedConnection: true,
+  },
+};
+const poolPromise = new sql.ConnectionPool(config)
+  poolPromise.connect()
+  .then((pool) => {
+    console.log('Connected to MYSQL');
+    return pool;
+  })
+  .catch((err) => console.log('Database Connection Failed! Bad Config: ', err));
+
+module.exports = {
+  sql,
+  poolPromise,
+};
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});*/
